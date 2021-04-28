3f10f536c940b603b323d0e91595a4a7d88a7754
Fabuzac <fabuzac.novenaire@hotmail.com>
2021-04-28 14:55:18 +0200

    ready to start

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,31 +1,38 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="keywords" content="" />
+	<meta name="description" content="" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>{{ config('app.name', 'Tweety') }}</title>
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/favicon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon.png">
+    <link rel="mask-icon" href="/favicon/favicon.png" color="#ff2d20">
+    <link rel="shortcut icon" href="favicon/favicon.png">
+    <meta name="theme-color" content="#ffffff">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css"/> --}}
+    @yield('head')
 </head>
 <body>
-    <div id="app">
+    <header>    
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Tweety') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -75,10 +82,16 @@
                 </div>
             </div>
         </nav>
+    </header>
+        @yield('header')
 
         <main class="py-4">
             @yield('content')
         </main>
-    </div>
+
+        @yield('footer')
+        <div id="copyright" class="has-text-centered container">
+            <p>&copy; 2021. All rights reserved. | Photos by <a href="#">Google</a> | Design by <a href="https://www.novation-web.com/" target="_blank" rel="nofollow">Novation-Web</a>.</p>
+        </div>
 </body>
 </html>
